e7a3477456fd79c05ed16071d291fe720dd8f67d
SAGAR PURKUTI <sagar101purkuti@gmail.com>
2025-12-29 22:14:35 +0545

    initaing project chatroom with django channels, websocket

---
+++ core/routing.py
@@ -0,0 +1,13 @@
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+import chat.routing
+from django.urls import path
+from chat import consumers
+
+application = ProtocolTypeRouter({
+    'websocket': AuthMiddlewareStack(
+        URLRouter([
+            chat.routing.websocket_urlpatterns
+        ])
+    ),
+})
--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'chat',
+    'channels'
 ]
 
 MIDDLEWARE = [
@@ -115,3 +117,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+ASGI_APPLICATION = 'core.routing.application'
